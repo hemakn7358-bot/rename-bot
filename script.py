f10ea30bb5fe69f30efbbf15e74218eaa3ac87c3
HKN <hemakn7358@gmail.com>
2026-03-25 11:45:44 +0530

    Add help text and commands for the bot

---
+++ script.py
@@ -0,0 +1,52 @@
+class script(object):
+    HELP_TXT = """<b>Hey</b> {}
+    
+<b>Here Is The Help For My Commands.</b>"""
+
+    CAPTION_TXT = """<b><u>📝  HOW TO SET CAPTION</u></b>
+
+<b>⦿ /set_caption - Use This Command To Set Your Caption</b>
+<b>⦿ /see_caption - Use This Command To See Your Caption</b>
+<b>⦿ /del_caption - Use This Command To Delete Your Caption</b>"""
+   
+    THUMBNAIL_TXT = """<b><u>🖼️  HOW TO SET THUMBNAIL</u></b>
+
+<b>⦿ You Can Add Custom Thumbnail Simply By Sending A Photo To Me....</b>
+
+<b>⦿ /viewthumb - Use This Command To See Your Thumbnail</b>
+<b>⦿ /delthumb - Use This Command To Delete Your Thumbnail</b>"""
+
+    ABOUT_TXT = """<b>🤖 My Name :</b> {}
+<b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
+<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
+<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
+<b>📢 Channel :</b> <a href='https://t.me/Anime_friend001'>Anime Tamil Friend</a>
+<b>🧑‍💻 Developer :</b> <a href='@ATF_Admin_Gojo'>Admine</a>
+
+<b>♻️ Bot Made By :</b> @ATF_Admin_Gojo"""
+
+
+    ADMIN_TXT = """<b><u>🦋 ADMIN ALL COMMANDS HERE</u></b>
+
+<b>⦿ /users - Use This Command To See Total Users</b>
+<b>⦿ /allids - Use This Command To See All Users IDs</b>
+<b>⦿ /broadcast - Use This Command To Send A Message To Users</b>
+<b>⦿ /warn - Use This Command To Send A Message To A User</b>
+<b>⦿ /resetpower - Use This Command To Reset User Power</b>
+<b>⦿ /ceasepower - Use This Command To Cease User Power</b>
+<b>⦿ /addpremium - Use This Command To Add Premium To Users</b>
+<b>⦿ /restart - Use This Command To Cancel All Process And Restart The Bot</b>"""
+
+
+    METADATA_TXT = """<b><u>🖼️  HOW TO SET CUSTOM METADATA</u></b>
+
+For Example :-
+
+<code>By @ATF_Admin_Gojo</code>
+
+💬 For Any Help Contact @ATF_Admin_Gojo
+"""
+
+
+
+
